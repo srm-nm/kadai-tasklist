fe6911018f1cb8c5245add369cac516236af21fe
ryo.sato <srm09.sh@gmail.com>
2021-07-21 18:34:48 +0900

    ページネーションの追加

--- src/main/java/controller/IndexServlet.java
+++ src/main/java/controller/IndexServlet.java
@@ -35,11 +35,25 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                             .setFirstResult(20 * (page - 1))
+                             .setMaxResults(20)
+                             .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTaskCount", Long.class)
+                                   .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
 
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -16,6 +16,10 @@ import javax.persistence.Table;
     @NamedQuery(
         name = "getAllTasks",
         query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+        ),
+    @NamedQuery(
+        name = "getTaskCount",
+        query = "SELECT COUNT(t) FROM Task AS t"
         )
 })
 @Table(name = "tasks")
